d382f70d8c0006230b5f3db62a2b7db7c0419a60
Waramet Hatjantuek <waramet.fl@gmail.com>
2024-01-04 18:57:28 +0700

    add completed exercise

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="font-sans flex justify-center text-4xl font-bold m-5 text-cyan-500">
+      เที่ยวไหนดี
+    </h1>
+  );
+}
+
+export default Header;
